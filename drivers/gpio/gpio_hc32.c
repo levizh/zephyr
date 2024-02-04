897ad03ed53cdc4f3d61569a802b73819cff7f80
greya <zhanglx@xhsc.com.cn>
2024-02-04 16:56:37 +0800

    format fix and add our's module path

--- drivers/gpio/gpio_hc32.c
+++ drivers/gpio/gpio_hc32.c
@@ -18,182 +18,182 @@
  * @brief Configure pin or port
  */
 static int gpio_hc32_configure(const struct device *port, gpio_pin_t pin,
-                            gpio_flags_t flags)
+			       gpio_flags_t flags)
 {
-    const struct gpio_hc32_config *cfg = port->config;;
-    uint8_t hc32_port = cfg->port;
-    stc_gpio_init_t stc_gpio_init;
+	const struct gpio_hc32_config *cfg = port->config;;
+	uint8_t hc32_port = cfg->port;
+	stc_gpio_init_t stc_gpio_init;
 
-    GPIO_StructInit(&stc_gpio_init);
-    /* figure out if we can map the requested GPIO
-     * configuration
-     */
+	GPIO_StructInit(&stc_gpio_init);
+	/* figure out if we can map the requested GPIO
+	 * configuration
+	 */
 
-    if ((flags & GPIO_OUTPUT) != 0U) {
-        /* ouput */
-        stc_gpio_init.u16PinDir = PIN_DIR_OUT;
-        if ((flags & GPIO_SINGLE_ENDED) != 0) {
-            if (flags & GPIO_LINE_OPEN_DRAIN) {
-                stc_gpio_init.u16PinOutputType = PIN_OUT_TYPE_NMOS;
-            } else  {
-                /* Output can't be open source */
-                return -ENOTSUP;
-            }
-        } else {
-            stc_gpio_init.u16PinOutputType = PIN_OUT_TYPE_CMOS;
-        }
+	if ((flags & GPIO_OUTPUT) != 0U) {
+		/* ouput */
+		stc_gpio_init.u16PinDir = PIN_DIR_OUT;
+		if ((flags & GPIO_SINGLE_ENDED) != 0) {
+			if (flags & GPIO_LINE_OPEN_DRAIN) {
+				stc_gpio_init.u16PinOutputType = PIN_OUT_TYPE_NMOS;
+			} else  {
+				/* Output can't be open source */
+				return -ENOTSUP;
+			}
+		} else {
+			stc_gpio_init.u16PinOutputType = PIN_OUT_TYPE_CMOS;
+		}
 
-        if (((flags & GPIO_PULL_UP) != 0) || ((flags & GPIO_PULL_DOWN) != 0)) {
-            /* No pull up or down in out mode*/
-            return -ENOTSUP;
-        }
+		if (((flags & GPIO_PULL_UP) != 0) || ((flags & GPIO_PULL_DOWN) != 0)) {
+			/* No pull up or down in out mode*/
+			return -ENOTSUP;
+		}
 
-        if ((flags & GPIO_OUTPUT_INIT_HIGH) != 0U) {
-            stc_gpio_init.u16PinState = PIN_STAT_SET;
-        } else if ((flags & GPIO_OUTPUT_INIT_LOW) != 0U) {
-            stc_gpio_init.u16PinState = PIN_STAT_RST;
-        } else if ((flags & GPIO_OUTPUT_INIT_LOGICAL) != 0U) {
-            /* Don't support logical set */
-            return -ENOTSUP;
-        }
-    } else if ((flags & GPIO_INPUT) != 0) {
-        /* Input */
-        stc_gpio_init.u16PinDir = PIN_DIR_IN;
+		if ((flags & GPIO_OUTPUT_INIT_HIGH) != 0U) {
+			stc_gpio_init.u16PinState = PIN_STAT_SET;
+		} else if ((flags & GPIO_OUTPUT_INIT_LOW) != 0U) {
+			stc_gpio_init.u16PinState = PIN_STAT_RST;
+		} else if ((flags & GPIO_OUTPUT_INIT_LOGICAL) != 0U) {
+			/* Don't support logical set */
+			return -ENOTSUP;
+		}
+	} else if ((flags & GPIO_INPUT) != 0) {
+		/* Input */
+		stc_gpio_init.u16PinDir = PIN_DIR_IN;
 
-        if ((flags & GPIO_PULL_UP) != 0) {
-            stc_gpio_init.u16PullUp = PIN_PU_ON;
-        } else if ((flags & GPIO_PULL_DOWN) != 0) {
-            /* No pull down */
-            return -ENOTSUP;
-        }
-    } else {
-        /* Deactivated: Analog */
-        stc_gpio_init.u16PinAttr = PIN_ATTR_ANALOG;
-    }
+		if ((flags & GPIO_PULL_UP) != 0) {
+			stc_gpio_init.u16PullUp = PIN_PU_ON;
+		} else if ((flags & GPIO_PULL_DOWN) != 0) {
+			/* No pull down */
+			return -ENOTSUP;
+		}
+	} else {
+		/* Deactivated: Analog */
+		stc_gpio_init.u16PinAttr = PIN_ATTR_ANALOG;
+	}
 
-    /* TODO:1,int edge(INTC module) */
-    GPIO_Init(hc32_port, BIT(pin), &stc_gpio_init);
+	/* TODO:1,int edge(INTC module) */
+	GPIO_Init(hc32_port, BIT(pin), &stc_gpio_init);
 
 #if 0
-    if ((flags & GPIO_INT_ENABLE) != 0) {
-        stc_gpio_init.u16ExtInt = PIN_EXTINT_ON;
-    }
+	if ((flags & GPIO_INT_ENABLE) != 0) {
+		stc_gpio_init.u16ExtInt = PIN_EXTINT_ON;
+	}
 #endif
-    return 0;
+	return 0;
 }
 
 static int gpio_hc32_get_config(const struct device *port, gpio_pin_t pin,
-                                gpio_flags_t flags)
+				gpio_flags_t flags)
 {
-    const struct gpio_hc32_config *cfg = port->config;;
-    uint16_t PCR_value = *cfg->base;
+	const struct gpio_hc32_config *cfg = port->config;;
+	uint16_t PCR_value = *(cfg->base + pin * 4);
 
-    if ((PCR_value & GPIO_PCR_DDIS) == 0U) {
-        if (((PCR_value) & GPIO_PCR_POUTE) != 0U) {
-            flags |= GPIO_OUTPUT;
-        } else {
-            flags |= GPIO_INPUT;
-            if (((PCR_value) & GPIO_PCR_PUU) != 0U){
-                flags |= GPIO_PULL_UP;
-            }
-        }
-    } else {
-        flags &= ~(GPIO_OUTPUT | GPIO_INPUT);
-    }
+	if ((PCR_value & GPIO_PCR_DDIS) == 0U) {
+		if (((PCR_value) & GPIO_PCR_POUTE) != 0U) {
+			flags |= GPIO_OUTPUT;
+		} else {
+			flags |= GPIO_INPUT;
+			if (((PCR_value) & GPIO_PCR_PUU) != 0U) {
+				flags |= GPIO_PULL_UP;
+			}
+		}
+	} else {
+		flags &= ~(GPIO_OUTPUT | GPIO_INPUT);
+	}
 
-    return flags;
+	return flags;
 }
 
 static int gpio_hc32_port_get_raw(const struct device *dev, uint32_t *value)
 {
-    const struct gpio_hc32_config *cfg = dev->config;
-    uint8_t port = cfg->port;
+	const struct gpio_hc32_config *cfg = dev->config;
+	uint8_t port = cfg->port;
 
-    *value = (uint32_t)GPIO_ReadInputPort(port);
+	*value = (uint32_t)GPIO_ReadInputPort(port);
 
-    return 0;
+	return 0;
 }
 
 static int gpio_hc32_port_set_masked_raw(const struct device *dev,
-					  gpio_port_pins_t mask,
-					  gpio_port_value_t value)
+					 gpio_port_pins_t mask,
+					 gpio_port_value_t value)
 {
-    const struct gpio_hc32_config *cfg = dev->config;
-    uint8_t port = cfg->port;
-    GPIO_WritePort(port, mask);
-     return 0;
+	const struct gpio_hc32_config *cfg = dev->config;
+	uint8_t port = cfg->port;
+	GPIO_WritePort(port, mask);
+	return 0;
 }
 
 static int gpio_hc32_port_set_bits_raw(const struct device *dev,
-					gpio_port_pins_t pins)
+				       gpio_port_pins_t pins)
 {
-    const struct gpio_hc32_config *cfg = dev->config;
-    uint8_t port = cfg->port;
-    GPIO_SetPins(port, pins);
-    return 0;
+	const struct gpio_hc32_config *cfg = dev->config;
+	uint8_t port = cfg->port;
+	GPIO_SetPins(port, pins);
+	return 0;
 }
 
 static int gpio_hc32_port_clear_bits_raw(const struct device *dev,
-					  gpio_port_pins_t pins)
+					 gpio_port_pins_t pins)
 {
-    const struct gpio_hc32_config *cfg = dev->config;
-    uint8_t port = cfg->port;
-    GPIO_ResetPins(port, pins);
-    return 0;
+	const struct gpio_hc32_config *cfg = dev->config;
+	uint8_t port = cfg->port;
+	GPIO_ResetPins(port, pins);
+	return 0;
 }
 
 static int gpio_hc32_port_toggle_bits(const struct device *dev,
-				       gpio_port_pins_t pins)
+				      gpio_port_pins_t pins)
 {
-    const struct gpio_hc32_config *cfg = dev->config;
-    uint8_t port = cfg->port;
-    GPIO_TogglePins(port, pins);
-    return 0;
+	const struct gpio_hc32_config *cfg = dev->config;
+	uint8_t port = cfg->port;
+	GPIO_TogglePins(port, pins);
+	return 0;
 }
 
 static int gpio_hc32_pin_interrupt_configure(const struct device *dev,
-					      gpio_pin_t pin,
-					      enum gpio_int_mode mode,
-					      enum gpio_int_trig trig)
+					     gpio_pin_t pin,
+					     enum gpio_int_mode mode,
+					     enum gpio_int_trig trig)
 {
-    /* FIXME: */
-    return 0;
+	/* FIXME: */
+	return 0;
 }
 
 static int gpio_hc32_manage_callback(const struct device *dev,
-                                     struct gpio_callback *callback,
-                                     bool set)
+				     struct gpio_callback *callback,
+				     bool set)
 {
-    struct gpio_hc32_data *data = dev->data;
+	struct gpio_hc32_data *data = dev->data;
 
-    return gpio_manage_callback(&data->cb, callback, set);
+	return gpio_manage_callback(&data->cb, callback, set);
 }
 
 static uint32_t gpio_hc32_get_pending_int(const struct device *dev)
 {
-    /* FIXME: */
-    return 0;
+	/* FIXME: */
+	return 0;
 }
 
 static const struct gpio_driver_api gpio_hc32_driver = {
-    .pin_configure = gpio_hc32_configure,
+	.pin_configure = gpio_hc32_configure,
 #if defined(CONFIG_GPIO_GET_CONFIG)
-    .pin_get_config = gpio_hc32_get_config,
+	.pin_get_config = gpio_hc32_get_config,
 #endif /* CONFIG_GPIO_GET_CONFIG */
-    .port_get_raw = gpio_hc32_port_get_raw,
-    .port_set_masked_raw = gpio_hc32_port_set_masked_raw,
-    .port_set_bits_raw = gpio_hc32_port_set_bits_raw,
-    .port_clear_bits_raw = gpio_hc32_port_clear_bits_raw,
-    .port_toggle_bits = gpio_hc32_port_toggle_bits,
-    .pin_interrupt_configure = gpio_hc32_pin_interrupt_configure,
-    .manage_callback = gpio_hc32_manage_callback,
-    .get_pending_int = gpio_hc32_get_pending_int,
+	.port_get_raw = gpio_hc32_port_get_raw,
+	.port_set_masked_raw = gpio_hc32_port_set_masked_raw,
+	.port_set_bits_raw = gpio_hc32_port_set_bits_raw,
+	.port_clear_bits_raw = gpio_hc32_port_clear_bits_raw,
+	.port_toggle_bits = gpio_hc32_port_toggle_bits,
+	.pin_interrupt_configure = gpio_hc32_pin_interrupt_configure,
+	.manage_callback = gpio_hc32_manage_callback,
+	.get_pending_int = gpio_hc32_get_pending_int,
 };
 
 
 static int gpio_hc32_init(const struct device *dev)
 {
-    return 0;
+	return 0;
 }
 
 #define GPIO_HC32_DEFINE(n) \
@@ -201,7 +201,7 @@ static int gpio_hc32_init(const struct device *dev)
 		.common = {						     \
 			.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_DT_INST(n),\
 		},														\
-		.base =	DT_INST_REG_ADDR(n),						\
+		.base =	(uint16_t*)DT_INST_REG_ADDR(n),						\
 		.port = n, 			\
 	};\
 	static struct gpio_hc32_data gpio_hc32_data_##n;	\
--- soc/arm/xhsc_hc32/hc32f4/soc.c
+++ soc/arm/xhsc_hc32/hc32f4/soc.c
@@ -25,56 +25,56 @@
  * clock.
  *
  */
-static int System_CLK_Init(struct device *arg)
+static int System_CLK_Init()
 {
-	ARG_UNUSED(arg);
-	
 	LL_PERIPH_WE(LL_PERIPH_ALL);
-	stc_clock_xtal_init_t     stcXtalInit;
-    stc_clock_pll_init_t      stcMpllInit;
+	stc_clock_xtal_init_t stcXtalInit;
+	stc_clock_pll_init_t      stcMpllInit;
 
-    GPIO_AnalogCmd(BSP_XTAL_PORT, BSP_XTAL_IN_PIN | BSP_XTAL_OUT_PIN, ENABLE);
-    (void)CLK_XtalStructInit(&stcXtalInit);
-    (void)CLK_PLLStructInit(&stcMpllInit);
+	GPIO_AnalogCmd(BSP_XTAL_PORT, BSP_XTAL_IN_PIN | BSP_XTAL_OUT_PIN, ENABLE);
+	(void)CLK_XtalStructInit(&stcXtalInit);
+	(void)CLK_PLLStructInit(&stcMpllInit);
 
-    /* Set bus clk div. */
-    CLK_SetClockDiv(CLK_BUS_CLK_ALL, (CLK_HCLK_DIV1 | CLK_EXCLK_DIV2 | CLK_PCLK0_DIV1 | CLK_PCLK1_DIV2 | \
-                                      CLK_PCLK2_DIV4 | CLK_PCLK3_DIV4 | CLK_PCLK4_DIV2));
+	/* Set bus clk div. */
+	CLK_SetClockDiv(CLK_BUS_CLK_ALL,
+			(CLK_HCLK_DIV1 | CLK_EXCLK_DIV2 | CLK_PCLK0_DIV1 | CLK_PCLK1_DIV2 | \
+			 CLK_PCLK2_DIV4 | CLK_PCLK3_DIV4 | CLK_PCLK4_DIV2));
 
-    /* Config Xtal and enable Xtal */
-    stcXtalInit.u8Mode = CLK_XTAL_MD_OSC;
-    stcXtalInit.u8Drv = CLK_XTAL_DRV_ULOW;
-    stcXtalInit.u8State = CLK_XTAL_ON;
-    stcXtalInit.u8StableTime = CLK_XTAL_STB_2MS;
-    (void)CLK_XtalInit(&stcXtalInit);
+	/* Config Xtal and enable Xtal */
+	stcXtalInit.u8Mode = CLK_XTAL_MD_OSC;
+	stcXtalInit.u8Drv = CLK_XTAL_DRV_ULOW;
+	stcXtalInit.u8State = CLK_XTAL_ON;
+	stcXtalInit.u8StableTime = CLK_XTAL_STB_2MS;
+	(void)CLK_XtalInit(&stcXtalInit);
 
-    /* MPLL config (XTAL / pllmDiv * plln / PllpDiv = 200M). */
-    stcMpllInit.PLLCFGR = 0UL;
-    stcMpllInit.PLLCFGR_f.PLLM = 1UL - 1UL;
-    stcMpllInit.PLLCFGR_f.PLLN = 50UL - 1UL;
-    stcMpllInit.PLLCFGR_f.PLLP = 2UL - 1UL;
-    stcMpllInit.PLLCFGR_f.PLLQ = 2UL - 1UL;
-    stcMpllInit.PLLCFGR_f.PLLR = 2UL - 1UL;
-    stcMpllInit.u8PLLState = CLK_PLL_ON;
-    stcMpllInit.PLLCFGR_f.PLLSRC = CLK_PLL_SRC_XTAL;
-    (void)CLK_PLLInit(&stcMpllInit);
-    /* Wait MPLL ready. */
-    while (SET != CLK_GetStableStatus(CLK_STB_FLAG_PLL)) {
-        ;
-    }
+	/* MPLL config (XTAL / pllmDiv * plln / PllpDiv = 200M). */
+	stcMpllInit.PLLCFGR = 0UL;
+	stcMpllInit.PLLCFGR_f.PLLM = 1UL - 1UL;
+	stcMpllInit.PLLCFGR_f.PLLN = 50UL - 1UL;
+	stcMpllInit.PLLCFGR_f.PLLP = 2UL - 1UL;
+	stcMpllInit.PLLCFGR_f.PLLQ = 2UL - 1UL;
+	stcMpllInit.PLLCFGR_f.PLLR = 2UL - 1UL;
+	stcMpllInit.u8PLLState = CLK_PLL_ON;
+	stcMpllInit.PLLCFGR_f.PLLSRC = CLK_PLL_SRC_XTAL;
+	(void)CLK_PLLInit(&stcMpllInit);
+	/* Wait MPLL ready. */
+	while (SET != CLK_GetStableStatus(CLK_STB_FLAG_PLL)) {
+		;
+	}
 
-    /* sram init include read/write wait cycle setting */
-    SRAM_SetWaitCycle(SRAM_SRAMH, SRAM_WAIT_CYCLE0, SRAM_WAIT_CYCLE0);
-    SRAM_SetWaitCycle((SRAM_SRAM12 | SRAM_SRAM3 | SRAM_SRAMR), SRAM_WAIT_CYCLE1, SRAM_WAIT_CYCLE1);
+	/* sram init include read/write wait cycle setting */
+	SRAM_SetWaitCycle(SRAM_SRAMH, SRAM_WAIT_CYCLE0, SRAM_WAIT_CYCLE0);
+	SRAM_SetWaitCycle((SRAM_SRAM12 | SRAM_SRAM3 | SRAM_SRAMR), SRAM_WAIT_CYCLE1,
+			  SRAM_WAIT_CYCLE1);
 
-    /* flash read wait cycle setting */
-    (void)EFM_SetWaitCycle(EFM_WAIT_CYCLE5);
-    /* 3 cycles for 126MHz ~ 200MHz */
-    GPIO_SetReadWaitCycle(GPIO_RD_WAIT3);
-    /* Switch driver ability */
-    (void)PWC_HighSpeedToHighPerformance();
-    /* Switch system clock source to MPLL. */
-    CLK_SetSysClockSrc(CLK_SYSCLK_SRC_PLL);
+	/* flash read wait cycle setting */
+	(void)EFM_SetWaitCycle(EFM_WAIT_CYCLE5);
+	/* 3 cycles for 126MHz ~ 200MHz */
+	GPIO_SetReadWaitCycle(GPIO_RD_WAIT3);
+	/* Switch driver ability */
+	(void)PWC_HighSpeedToHighPerformance();
+	/* Switch system clock source to MPLL. */
+	CLK_SetSysClockSrc(CLK_SYSCLK_SRC_PLL);
 
 	return 0;
 }

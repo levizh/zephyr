a38a3fd7c4f066fb9dc66d4f18e2d50df92c9341
liangchuan <liangchuan@xhsc.com.cn>
2024-02-29 16:51:01 +0800

    dma driver implement some macro for get config.

--- include/zephyr/drivers/dma/dma_hc32.h
+++ include/zephyr/drivers/dma/dma_hc32.h
@@ -7,10 +7,24 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_DMA_HC32_H_
 #define ZEPHYR_INCLUDE_DRIVERS_DMA_HC32_H_
 
+#define HC32_DMA_CTLR(id, dir)						\
+		DT_INST_DMAS_CTLR_BY_NAME(id, dir)
+
+#define HC32_DMA_SLOT(id, dir, slot)				\
+		DT_INST_DMAS_CELL_BY_NAME(id, dir, slot)
+
+#define HC32_DMA_CHANNEL_CONFIG(id, dir)					\
+		DT_INST_DMAS_CELL_BY_NAME(id, dir, channel_config)
+
+/* ref dma.h */
+
+/* 0 -> MEM_TO_MEM, 1 -> MEM_TO_PERIPH, 2 -> PERIPH_TO_MEM */
 #define HC32_DMA_CONFIG_DIRECTION(config)			  ((config >> 4) & 0x3)
+/* 0 -> inc, 1 -> dec, 2 -> no change */
 #define HC32_DMA_CONFIG_SOURCE_ADDR_INC(config)		  ((config >> 6) & 0x3)
 #define HC32_DMA_CONFIG_DEST_ADDR_INC(config)	      ((config >> 8) & 0x3)
-#define HC32_DMA_CONFIG_DATA_WIDTH(config)	          ((config >> 10) & 0x3)
+/* 0 -> 1 byte, 1 -> 2 bytes, 2 -> 4 bytes */
+#define HC32_DMA_CONFIG_DATA_WIDTH(config)	          (1 << ((config >> 10) & 0x3))
 
 struct dma_hc32_config_user_data {
 	uint32_t slot; /* hc32 mcu uses this value to store the aos source that triggered the dma */
--- include/zephyr/dt-bindings/dma/hc32_dma.h
+++ include/zephyr/dt-bindings/dma/hc32_dma.h
@@ -24,15 +24,15 @@
 
 /* source address increase defined on bits 6-7 */
 #define HC32_DMA_CH_CFG_SOURCE_ADDR_INC(val) ((val & 0x3) << 6)
-#define HC32_DMA_SOURCE_ADDR_FIX             HC32_DMA_CH_CFG_SOURCE_ADDR_INC(0)
-#define HC32_DMA_SOURCE_ADDR_INC             HC32_DMA_CH_CFG_SOURCE_ADDR_INC(1)
-#define HC32_DMA_SOURCE_ADDR_DEC             HC32_DMA_CH_CFG_SOURCE_ADDR_INC(2)
+#define HC32_DMA_SOURCE_ADDR_INC             HC32_DMA_CH_CFG_SOURCE_ADDR_INC(0)
+#define HC32_DMA_SOURCE_ADDR_DEC             HC32_DMA_CH_CFG_SOURCE_ADDR_INC(1)
+#define HC32_DMA_SOURCE_ADDR_FIX             HC32_DMA_CH_CFG_SOURCE_ADDR_INC(2)
 
 /* dest address increase defined on bits 8-9 */
 #define HC32_DMA_CH_CFG_DEST_ADDR_INC(val)  ((val & 0x3) << 8)
-#define HC32_DMA_DEST_ADDR_FIX              HC32_DMA_CH_CFG_DEST_ADDR_INC(0)
-#define HC32_DMA_DEST_ADDR_INC              HC32_DMA_CH_CFG_DEST_ADDR_INC(1)
-#define HC32_DMA_DEST_ADDR_DEC              HC32_DMA_CH_CFG_DEST_ADDR_INC(2)
+#define HC32_DMA_DEST_ADDR_INC              HC32_DMA_CH_CFG_DEST_ADDR_INC(0)
+#define HC32_DMA_DEST_ADDR_DEC              HC32_DMA_CH_CFG_DEST_ADDR_INC(1)
+#define HC32_DMA_DEST_ADDR_FIX              HC32_DMA_CH_CFG_DEST_ADDR_INC(2)
 
 /* data size defined on bits 10-11 */
 #define HC32_DMA_CH_CFG_DATA_WIDTH(val)     ((val & 0x3) << 10)

cdfc9cb16dd71fb2167558e4886be8b2a3afcb0a
pengbiao <pengbiao@xhsc.com.cn>
2024-03-15 10:35:29 +0800

    1.add dma support for i2c driver and bus restore

--- drivers/i2c/i2c_hc32.h
+++ drivers/i2c/i2c_hc32.h
@@ -19,19 +19,25 @@ struct i2c_hc32_config {
 	irq_config_func_t irq_config_func;
 #endif
 #ifdef CONFIG_I2C_HC32_DMA
+	const struct device *dma_dev[2];
+	struct dma_config *dma_conf;
+	uint8_t channel[2];
 	dma_callback_t dma_callbcck;
 #endif
 	const struct hc32_modules_clock_sys *mod_clk;
 	CM_I2C_TypeDef *i2c;
+
 	uint32_t bitrate;
 	const struct pinctrl_dev_config *pcfg;
+#ifdef CONFIG_I2C_HC32_BUS_RECOVERY
 	struct gpio_dt_spec scl;
 	struct gpio_dt_spec sda;
+#endif
 	uint32_t time_out;
 };
 
 struct i2c_hc32_data {
-#ifdef CONFIG_I2C_HC32_INTERRUPT
+#if defined (CONFIG_I2C_HC32_INTERRUPT) || (CONFIG_I2C_HC32_DMA)
 	struct k_sem device_sync_sem;
 #endif
 	struct k_sem bus_mutex;
@@ -47,8 +53,5 @@ int32_t hc32_i2c_transaction(const struct device *dev,
 			    struct i2c_msg msg, uint8_t *next_msg_flags,
 			    uint16_t periph);
 int i2c_hc32_runtime_configure(const struct device *dev, uint32_t config);
-
-void hc32_i2c_event_isr(void *arg);
-void hc32_i2c_error_isr(void *arg);
 
 #endif	/* ZEPHYR_DRIVERS_I2C_I2C_HC32_H_ */

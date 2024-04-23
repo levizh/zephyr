96dd0407caf4c6ba61eda19509d3c262fcfe0ad1
liangchuan <liangchuan@xhsc.com.cn>
2024-04-23 10:23:05 +0800

    fix counter complie err when rtc node disable in dts.

--- soc/arm/xhsc_hc32/hc32f4/dts_fixup.h
+++ soc/arm/xhsc_hc32/hc32f4/dts_fixup.h
@@ -8,7 +8,9 @@
 
 #define DT_NUM_IRQ_PRIO_BITS            DT_ARM_V7M_NVIC_E000E100_ARM_NUM_IRQ_PRIORITY_BITS
 
+#if defined(DT_XHSC_HC32_RTC_0_LABEL)
 #define DT_RTC_0_NAME                   DT_XHSC_HC32_RTC_0_LABEL
+#endif
 
 #if defined(DT_XHSC_HC32_CAN_40070400_LABEL)
 #define DT_CAN_1_NAME                   DT_XHSC_HC32_CAN_40070400_LABEL

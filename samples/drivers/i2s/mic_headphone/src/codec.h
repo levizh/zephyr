f809a991d9622a40d7c91abe501b2088035ba41e
pengbiao <pengbiao@xhsc.com.cn>
2024-07-26 16:30:15 +0800

    Add i2s driver wm8731 sample

---
+++ samples/drivers/i2s/mic_headphone/src/codec.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2022-2024, Xiaohua Semiconductor Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define WM8731_NODE DT_NODELABEL(wm8731)
+
+bool init_wm8731_i2c(void);

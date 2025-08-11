16989d8cf756fa1bc478cc5566f8fd10a4e5ae91
TrackingRobot <code9527@126.com>
2025-08-11 15:22:31 +0800

    drivers: gpio: refactor this function to reduce its cognitive complexity
    Signed-off-by: TrackingRobot <code9527@126.com>

--- drivers/gpio/gpio_hc32.c
+++ drivers/gpio/gpio_hc32.c
@@ -30,11 +30,93 @@ static void gpio_hc32_isr(int line, void *arg)
 }
 
 /**
+ * @brief Calculate the on/off status of the exit interrupt by the given flags
+ *
+ */
+static int gpio_hc32_cfg2ll_intr_on(gpio_flags_t flags)
+{
+	int ext_int = PIN_EXTINT_OFF;
+
+	if ((flags & GPIO_INT_MASK) != 0) {
+		if ((flags & GPIO_INT_ENABLE) != 0) {
+			ext_int = PIN_EXTINT_ON;
+		}
+	}
+
+	return ext_int;
+}
+
+/**
+ * @brief Calculate interrupt trigger type by the given flags
+ *
+ */
+static int gpio_hc32_cfg2ll_intr_trig(gpio_flags_t flags)
+{
+	int trigger = HC32_EXTINT_TRIG_NOT_SUPPT;
+
+	if ((flags & GPIO_INT_EDGE_BOTH) == GPIO_INT_EDGE_BOTH) {
+		trigger = HC32_EXTINT_TRIG_BOTH;
+	} else if ((flags & GPIO_INT_EDGE_RISING) == GPIO_INT_EDGE_RISING) {
+		trigger = HC32_EXTINT_TRIG_RISING;
+	} else if ((flags & GPIO_INT_EDGE_FALLING) == GPIO_INT_EDGE_FALLING) {
+		trigger = HC32_EXTINT_TRIG_FALLING;
+	} else if ((flags & GPIO_INT_LEVEL_LOW) == GPIO_INT_LEVEL_LOW) {
+		trigger = HC32_EXTINT_TRIG_LOW_LVL;
+	} else {
+		/* Nothing to do */
+	}
+
+	return trigger;
+}
+
+/**
+ * @brief Calculate gpio output type
+ *
+ */
+static int gpio_hc32_cfg2ll_output_type(gpio_flags_t flags, uint16_t *out_type)
+{
+	if ((flags & GPIO_SINGLE_ENDED) != 0) {
+		if (flags & GPIO_LINE_OPEN_DRAIN) {
+			*out_type = PIN_OUT_TYPE_NMOS;
+		} else {
+			/* Output can't be open source */
+			return -ENOTSUP;
+		}
+	} else {
+		*out_type = PIN_OUT_TYPE_CMOS;
+	}
+
+	return 0;
+}
+
+/**
+ * @brief Calculate gpio output status
+ *
+ */
+static int
+	gpio_hc32_cfg2ll_output_status(gpio_flags_t flags, uint16_t *out_status)
+{
+	if ((flags & GPIO_OUTPUT_INIT_HIGH) != 0U) {
+		*out_status = PIN_STAT_SET;
+	} else if ((flags & GPIO_OUTPUT_INIT_LOW) != 0U) {
+		*out_status = PIN_STAT_RST;
+	} else if ((flags & GPIO_OUTPUT_INIT_LOGICAL) != 0U) {
+		/* Don't support logical set */
+		return -ENOTSUP;
+	} else {
+		/* Nothing to do */
+	}
+
+	return 0;
+}
+
+/**
  * @brief Configure pin or port
  */
 static int gpio_hc32_configure(const struct device *port, gpio_pin_t pin,
-			       gpio_flags_t flags)
+				gpio_flags_t flags)
 {
+	int intr_trigger;
 	const struct gpio_hc32_config *cfg = port->config;;
 	uint8_t hc32_port = cfg->port;
 	stc_gpio_init_t stc_gpio_init;
@@ -45,15 +127,10 @@ static int gpio_hc32_configure(const struct device *port, gpio_pin_t pin,
 	if ((flags & GPIO_OUTPUT) != 0U) {
 		/* ouput */
 		stc_gpio_init.u16PinDir = PIN_DIR_OUT;
-		if ((flags & GPIO_SINGLE_ENDED) != 0) {
-			if (flags & GPIO_LINE_OPEN_DRAIN) {
-				stc_gpio_init.u16PinOutputType = PIN_OUT_TYPE_NMOS;
-			} else  {
-				/* Output can't be open source */
-				return -ENOTSUP;
-			}
-		} else {
-			stc_gpio_init.u16PinOutputType = PIN_OUT_TYPE_CMOS;
+
+		if (0 != gpio_hc32_cfg2ll_output_type(flags,
+				&stc_gpio_init.u16PinOutputType)) {
+			return -ENOTSUP;
 		}
 
 		if (((flags & GPIO_PULL_UP) != 0) || ((flags & GPIO_PULL_DOWN) != 0)) {
@@ -61,11 +138,8 @@ static int gpio_hc32_configure(const struct device *port, gpio_pin_t pin,
 			return -ENOTSUP;
 		}
 
-		if ((flags & GPIO_OUTPUT_INIT_HIGH) != 0U) {
-			stc_gpio_init.u16PinState = PIN_STAT_SET;
-		} else if ((flags & GPIO_OUTPUT_INIT_LOW) != 0U) {
-			stc_gpio_init.u16PinState = PIN_STAT_RST;
-		} else if ((flags & GPIO_OUTPUT_INIT_LOGICAL) != 0U) {
+		if (0 != gpio_hc32_cfg2ll_output_status(flags,
+				&stc_gpio_init.u16PinState)) {
 			/* Don't support logical set */
 			return -ENOTSUP;
 		}
@@ -78,6 +152,8 @@ static int gpio_hc32_configure(const struct device *port, gpio_pin_t pin,
 		} else if ((flags & GPIO_PULL_DOWN) != 0) {
 			/* No pull down */
 			return -ENOTSUP;
+		} else {
+			/* Nothing to do */
 		}
 	} else {
 		/* Deactivated: Analog */
@@ -85,26 +161,14 @@ static int gpio_hc32_configure(const struct device *port, gpio_pin_t pin,
 	}
 
 	/* GPIO interrupt configuration flags */
-	if ((flags & GPIO_INT_MASK) != 0) {
-		if ((flags & GPIO_INT_ENABLE) != 0) {
-			stc_gpio_init.u16ExtInt = PIN_EXTINT_ON;
-		} else if ((flags & GPIO_INT_DISABLE) != 0) {
-			stc_gpio_init.u16ExtInt = PIN_EXTINT_OFF;
-		}
-
-		if ((flags & GPIO_INT_EDGE_BOTH) == GPIO_INT_EDGE_BOTH) {
-			hc32_extint_trigger(pin, HC32_EXTINT_TRIG_BOTH);
-		} else if ((flags & GPIO_INT_EDGE_RISING) == GPIO_INT_EDGE_RISING) {
-			hc32_extint_trigger(pin, HC32_EXTINT_TRIG_RISING);
-		} else if ((flags & GPIO_INT_EDGE_FALLING) == GPIO_INT_EDGE_FALLING) {
-			hc32_extint_trigger(pin, HC32_EXTINT_TRIG_FALLING);
-		} else if ((flags & GPIO_INT_LEVEL_LOW) == GPIO_INT_LEVEL_LOW) {
-			hc32_extint_trigger(pin, HC32_EXTINT_TRIG_LOW_LVL);
-		} else if (((flags & GPIO_INT_LEVEL_HIGH) == GPIO_INT_LEVEL_HIGH)
-			   || ((flags & GPIO_INT_LEVELS_LOGICAL) != 0)) {
-			/* Not support hight level and logical level set for int */
+	stc_gpio_init.u16ExtInt = gpio_hc32_cfg2ll_intr_on(flags);
+	if (PIN_EXTINT_ON == stc_gpio_init.u16ExtInt) {
+		intr_trigger = gpio_hc32_cfg2ll_intr_trig(flags);
+		if (HC32_EXTINT_TRIG_NOT_SUPPT == intr_trigger) {
 			stc_gpio_init.u16ExtInt = PIN_EXTINT_OFF;
 			return -ENOTSUP;
+		} else {
+			hc32_extint_trigger(pin, intr_trigger);
 		}
 	}
 
@@ -158,8 +222,8 @@ static int gpio_hc32_port_get_raw(const struct device *dev, uint32_t *value)
 }
 
 static int gpio_hc32_port_set_masked_raw(const struct device *dev,
-					 gpio_port_pins_t mask,
-					 gpio_port_value_t value)
+				gpio_port_pins_t mask,
+				gpio_port_value_t value)
 {
 	const struct gpio_hc32_config *cfg = dev->config;
 	uint8_t port = cfg->port;
@@ -168,7 +232,7 @@ static int gpio_hc32_port_set_masked_raw(const struct device *dev,
 }
 
 static int gpio_hc32_port_set_bits_raw(const struct device *dev,
-				       gpio_port_pins_t pins)
+				gpio_port_pins_t pins)
 {
 	const struct gpio_hc32_config *cfg = dev->config;
 	uint8_t port = cfg->port;
@@ -177,7 +241,7 @@ static int gpio_hc32_port_set_bits_raw(const struct device *dev,
 }
 
 static int gpio_hc32_port_clear_bits_raw(const struct device *dev,
-					 gpio_port_pins_t pins)
+				gpio_port_pins_t pins)
 {
 	const struct gpio_hc32_config *cfg = dev->config;
 	uint8_t port = cfg->port;
@@ -186,7 +250,7 @@ static int gpio_hc32_port_clear_bits_raw(const struct device *dev,
 }
 
 static int gpio_hc32_port_toggle_bits(const struct device *dev,
-				      gpio_port_pins_t pins)
+					gpio_port_pins_t pins)
 {
 	const struct gpio_hc32_config *cfg = dev->config;
 	uint8_t port = cfg->port;
@@ -195,9 +259,9 @@ static int gpio_hc32_port_toggle_bits(const struct device *dev,
 }
 
 static int gpio_hc32_pin_interrupt_configure(const struct device *dev,
-					     gpio_pin_t pin,
-					     enum gpio_int_mode mode,
-					     enum gpio_int_trig trig)
+						gpio_pin_t pin,
+						enum gpio_int_mode mode,
+						enum gpio_int_trig trig)
 {
 	const struct gpio_hc32_config *cfg = dev->config;
 	uint8_t port = cfg->port;
@@ -211,6 +275,8 @@ static int gpio_hc32_pin_interrupt_configure(const struct device *dev,
 	} else if (mode == GPIO_INT_MODE_ENABLE_ONLY) {
 		hc32_extint_enable(port, pin);
 		return 0;
+	} else {
+		/* Nothing to do */
 	}
 #endif /* CONFIG_GPIO_ENABLE_DISABLE_INTERRUPT */
 
@@ -256,8 +322,8 @@ static int gpio_hc32_pin_interrupt_configure(const struct device *dev,
 }
 
 static int gpio_hc32_manage_callback(const struct device *dev,
-				     struct gpio_callback *callback,
-				     bool set)
+					struct gpio_callback *callback,
+					bool set)
 {
 	struct gpio_hc32_data *data = dev->data;
 
@@ -295,16 +361,16 @@ static int gpio_hc32_init(const struct device *dev)
 
 #define GPIO_HC32_DEFINE(n) \
 	static const struct gpio_hc32_config gpio_hc32_cfg_##n = {	\
-		.common = {						     \
+		.common = {												\
 			.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_DT_INST(n),\
 		},														\
-		.base =	(uint16_t*)DT_INST_REG_ADDR(n),						\
-		.port = n, 			\
-	};\
-	static struct gpio_hc32_data gpio_hc32_data_##n;	\
-	DEVICE_DT_INST_DEFINE(n, gpio_hc32_init, NULL,     \
-	        &gpio_hc32_data_##n, &gpio_hc32_cfg_##n,     \
-	        POST_KERNEL, CONFIG_GPIO_INIT_PRIORITY,     \
-	        &gpio_hc32_driver);
+		.base =	(uint16_t*)DT_INST_REG_ADDR(n),					\
+		.port = n,												\
+	};															\
+	static struct gpio_hc32_data gpio_hc32_data_##n;			\
+	DEVICE_DT_INST_DEFINE(n, gpio_hc32_init, NULL,				\
+			&gpio_hc32_data_##n, &gpio_hc32_cfg_##n,			\
+			POST_KERNEL, CONFIG_GPIO_INIT_PRIORITY,				\
+			&gpio_hc32_driver);
 
 DT_INST_FOREACH_STATUS_OKAY(GPIO_HC32_DEFINE)
